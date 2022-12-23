da1df564b97d39260b6c46cec85b4177ad398922
Kurtana13 <sabakurtanidze23@gmail.com>
2022-12-24 01:21:28 +0400

    Initial

---
+++ Library/LibraryShared/Book.cs
@@ -0,0 +1,20 @@
+﻿using LibraryShared.CustomValidations;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.CompilerServices;
+
+namespace LibraryShared
+{
+    public class Book
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Title is required for the book!")]
+        [MaxLength(50,ErrorMessage ="Title of the book is too long!")]
+        public string Title { get; set; }
+        [Required]
+        public string Author { get; set; }
+        [Required]
+        [ValidDate(ErrorMessage ="Date of publication is wrong!")]
+        public DateTime DateOfPublication { get; set; }
+    }
+}
---
+++ Library/LibraryShared/CustomValidations/ValidDate.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryShared.CustomValidations
+{
+    internal class ValidDate :ValidationAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            DateTime date = (DateTime)value;
+            return date < DateTime.Now; 
+        }
+    }
+}
